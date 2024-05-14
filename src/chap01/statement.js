52ae595f5b6e57fe542daf6a4ccf71e24f01a4da
StrongWise <ghoul300@naver.com>
2024-05-14 16:27:16 +0900

    refactor: move playFor function

--- src/chap01/statement.js
+++ src/chap01/statement.js
@@ -1,14 +1,24 @@
 export function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
-  return renderPlainText(statementData, plays);
+  statementData.performances = invoice.performances.map(enrichPerformance);
+  return renderPlainText(statementData);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    return result;
+  }
+
+  function playFor(perf) {
+    return plays[perf.playID];
+  }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
@@ -41,20 +51,16 @@ function renderPlainText(data, plays) {
   function volumeCreditsFor(perf) {
     let result = 0;
     result += Math.max(perf.audience - 30, 0);
-    if ('comedy' === playFor(perf).type) {
+    if ('comedy' === perf.play.type) {
       result += Math.floor(perf.audience / 5);
     }
     return result;
   }
 
-  function playFor(perf) {
-    return plays[perf.playID];
-  }
-
   function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case 'tragedy': // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -69,7 +75,7 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
     return result;
   }
