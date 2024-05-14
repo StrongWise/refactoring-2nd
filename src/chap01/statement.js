45395d9c4c2e1abcb6579c1662e7982d8516c6fc
StrongWise <ghoul300@naver.com>
2024-05-14 15:56:14 +0900

    refactor: change function declaration

--- src/chap01/statement.js
+++ src/chap01/statement.js
@@ -8,10 +8,10 @@ export function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case 'tragedy': // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -26,7 +26,7 @@ export function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
     return result;
   }
@@ -36,7 +36,7 @@ export function statement(invoice, plays) {
   }
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
