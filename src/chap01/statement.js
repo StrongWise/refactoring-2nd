480d17e12dbf95c1e69d0a7b7c8e01d00285cd18
StrongWise <ghoul300@naver.com>
2024-05-14 16:08:02 +0900

    refactor: extract totalVolumeCredits function

--- src/chap01/statement.js
+++ src/chap01/statement.js
@@ -1,6 +1,4 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   function usd(aNumber) {
@@ -47,16 +45,23 @@ export function statement(invoice, plays) {
     return volumeCredits;
   }
 
+  let totalAmount = 0;
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
 
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
+
   // return
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
