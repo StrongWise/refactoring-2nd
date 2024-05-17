56c0b2a6e64ec9da64a63b6dfc28c15317fa83fe
StrongWise <ghoul300@naver.com>
2024-05-17 17:41:52 +0900

    test: 매개변수 추가하기

--- test/chap06/change-function-declaration.test.js
+++ test/chap06/change-function-declaration.test.js
@@ -1,9 +1,15 @@
 import {expect} from "chai";
-import {circumference} from "../../src/chap06/change-function-declaration.js";
+import {Book, circumference} from "../../src/chap06/change-function-declaration.js";
 
 describe('change-function-declaration', () => {
   it('circumference', () => {
     const radius = 3;
     expect(circumference(radius)).equal(2 * Math.PI * radius);
+  })
+  it('Book', () => {
+    const book = new Book();
+    book.addReservation({name:'StrongWise', date: '20240517'})
+    expect(book.reservations[0].name).equal('StrongWise');
+    expect(book.reservations[0].date).equal('20240517');
   })
 })
