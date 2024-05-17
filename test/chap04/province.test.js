55271f21bcfcb3750b437d04f7f756006cc905f6
StrongWise <ghoul300@naver.com>
2024-05-17 09:59:01 +0900

    test: province shortfall

--- test/chap04/province.test.js
+++ test/chap04/province.test.js
@@ -3,7 +3,6 @@ import { assert } from 'chai';
 import {Province, sampleProvinceData} from '../../src/chap04/province.js';
 
 describe('province', () => {
-	const asia = new Province(sampleProvinceData()); // 이렇게 하면 안됨
 	it('shortfall', () => {
     const asia = new Province(sampleProvinceData()); // 이렇게 하면 안됨
 		assert.equal(asia.shortfall, 5)
