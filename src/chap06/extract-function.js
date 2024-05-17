5b88ad13f80a607589ed5e73eff0b03d88815d9f
StrongWise <ghoul300@naver.com>
2024-05-17 15:47:44 +0900

    refactor: 마감일 설정 로직을 함수로 추출

--- src/chap06/extract-function.js
+++ src/chap06/extract-function.js
@@ -8,11 +8,13 @@ export function printOwing(invoice) {
 		outstanding += o.amount;
 	}
 
-	// 마감일(dueDate)을 기록한다.
-	const today = Clock.today;
-	invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
+  recordDueDate(invoice); // 마감일 설정 로직을 함수로 추출
   printDetails(invoice, outstanding); // 세부 사항 출력 로직을 함수로 추출
+}
+
+function recordDueDate(invoice) {
+  const today = Clock.today;
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
 
 function printDetails(invoice, outstanding) {
