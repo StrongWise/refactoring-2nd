8b6eccff81bc1e554e2df899df8894c7bcebb000
StrongWise <ghoul300@naver.com>
2024-05-17 16:30:30 +0900

    test: inline function 2

--- test/chap06/inline-function.test.js
+++ test/chap06/inline-function.test.js
@@ -1,9 +1,14 @@
 import {expect} from 'chai';
-import {rating} from '../../src/chap06/inline-function.js';
+import {rating, reportLines} from '../../src/chap06/inline-function.js';
 
 describe('inline-function', () => {
   it('rating', () => {
     expect(rating({numberOfLateDeliveries: 6})).to.equal(2);
     expect(rating({numberOfLateDeliveries: 5})).to.equal(1);
+  });
+  it('reportLines', () => {
+    const result = reportLines({name: 'StrongWise', location:'Seoul'});
+    expect(result[0][1]).to.equal('StrongWise');
+    expect(result[1][1]).to.equal('Seoul');
   });
 });
