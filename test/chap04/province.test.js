67349a53d46324675f980b24b5f34045239850b8
StrongWise <ghoul300@naver.com>
2024-05-17 11:29:54 +0900

    test: 경계 조건 검사

--- test/chap04/province.test.js
+++ test/chap04/province.test.js
@@ -9,18 +9,39 @@ describe('province', () => {
 		asia = new Province(sampleProvinceData());
 	});
 	it('shortfall', () => {
+    // assert.equal(asia.shortfall, 5);
     expect(asia.shortfall).equal(5);
 	});
 	it('profit', () => {
 		expect(asia.profit).equal(230);
 	});
+  // 픽스처 수정하기
 	it('change production', () => {
 		asia.producers[0].production = 20;
 		expect(asia.shortfall).equal(-6);
 		expect(asia.profit).equal(292);
 	});
+  // 경계 조건 검사 : 수요가 없다.
+	it('zero demand', () => {
+		asia.demand = 0;
+		expect(asia.shortfall).equal(-25);
+		expect(asia.profit).equal(0);
+	});
+  // 경계 조건 검사 : 수요가 마이너스다.
+	it('negative demand', () => {
+		asia.demand = -1;
+		expect(asia.shortfall).equal(-26);
+		expect(asia.profit).equal(-10);
+	});
+  // 경계 조건 검사 : 수요 입력란이 비어있다.
+	it('empty string demand', () => {
+		asia.demand = '';
+		expect(asia.shortfall).NaN;
+		expect(asia.profit).NaN;
+	});
 });
 
+// 경계 조건 검사 : 생산자가 없다.
 describe('no producers', () => {
 	let noProducers;
 	beforeEach(() => {
@@ -37,5 +58,18 @@ describe('no producers', () => {
 	});
 	it('profit', () => {
 		expect(noProducers.profit).equal(0);
+	});
+});
+
+describe('string for producers', () => {
+	it('', () => {
+		const data = {
+			name: 'String producers',
+			producers: '',
+			demand: 30,
+			price: 20,
+		};
+		const prov = new Province(data);
+		expect(prov.shortfall).equal(0);
 	});
 });
