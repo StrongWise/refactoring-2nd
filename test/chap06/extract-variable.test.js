76457bb76eea60c2040556e071690fc9e8a889ce
StrongWise <ghoul300@naver.com>
2024-05-17 16:47:39 +0900

    test: extract variable in class

--- test/chap06/extract-variable.test.js
+++ test/chap06/extract-variable.test.js
@@ -1,8 +1,12 @@
 import {expect} from "chai";
-import {price} from "../../src/chap06/extract-variable.js";
+import {Order, price} from "../../src/chap06/extract-variable.js";
 
 describe('extract-variable', () => {
   it('price', () => {
     expect(price({quantity: 20, itemPrice: 400})).to.equal(8100);
+  });
+  it('priceOrderClass', () => {
+    const order = new Order({quantity: 20, itemPrice: 400});
+    expect(order.price).to.equal(8100);
   });
 });
