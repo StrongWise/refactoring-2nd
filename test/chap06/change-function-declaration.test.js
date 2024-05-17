1629d614323120f5b6fe81f329d1c75c7c8f89bb
StrongWise <ghoul300@naver.com>
2024-05-17 17:29:01 +0900

    test: 함수 이름 바꾸기

---
+++ test/chap06/change-function-declaration.test.js
@@ -0,0 +1,13 @@
+import {expect} from "chai";
+import {circum, circumference} from "../../src/chap06/change-function-declaration.js";
+
+describe('change-function-declaration', () => {
+  it('circum', () => {
+    const radius = 3;
+    expect(circum(radius)).equal(2 * Math.PI * radius);
+  })
+  it('circumference', () => {
+    const radius = 3;
+    expect(circum(radius)).equal(2 * Math.PI * radius);
+  })
+})
