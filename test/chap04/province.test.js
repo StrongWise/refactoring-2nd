6370d5f903ddcd32022d221cf3ba85ef2f775668
StrongWise <ghoul300@naver.com>
2024-05-17 10:43:21 +0900

    test: 표준 픽스처 기반 profit test 추가

--- test/chap04/province.test.js
+++ test/chap04/province.test.js
@@ -1,11 +1,16 @@
-import { assert } from 'chai';
-import { describe, it } from 'mocha';
-
+import { expect } from 'chai';
 import {Province, sampleProvinceData} from '../../src/chap04/province.js';
 
 describe('province', () => {
+	let asia;
+	beforeEach(() => {
+		// beforeEach는 각각의 테스트 바로 전에 실행되어 asia를 초기화하므로 모든 테스트가 자신만의 새로운 asia를 사용하게 된다.
+		asia = new Province(sampleProvinceData());
+	});
 	it('shortfall', () => {
-    const asia = new Province(sampleProvinceData());
-		assert.equal(asia.shortfall, 5)
+    expect(asia.shortfall).equal(5);
+	});
+	it('profit', () => {
+		expect(asia.profit).equal(230);
 	});
 });
