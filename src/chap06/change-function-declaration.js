b92886baef70039ab1b2723f8ab2adc5031ba95c
StrongWise <ghoul300@naver.com>
2024-05-17 17:44:03 +0900

    refactor: extract function

--- src/chap06/change-function-declaration.js
+++ src/chap06/change-function-declaration.js
@@ -13,6 +13,10 @@ export class Book {
   }
 
   addReservation(customer) {
+    this.zz_addReservation(customer);
+  }
+
+  zz_addReservation(customer) {
     this._reservations.push(customer);
   }
 }
