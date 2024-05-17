63532cd2f3d5d1d53cfdc022fbb8cac50d03d17b
StrongWise <ghoul300@naver.com>
2024-05-17 16:50:16 +0900

    refactor: extract variable in class 3

--- src/chap06/extract-variable.js
+++ src/chap06/extract-variable.js
@@ -15,11 +15,6 @@ export class Order {
   get itemPrice() {return this._data.itemPrice;}
   get basePrice() {return this.quantity * this.itemPrice;}
   get quantityDiscount() {return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;}
-
-
-  get price() {
-    return this.basePrice -
-      this.quantityDiscount +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100);
-  }
+  get shipping() {return Math.min(this.quantity * this.itemPrice * 0.1, 100);}
+  get price() {return this.basePrice - this.quantityDiscount + this.shipping;}
 }
