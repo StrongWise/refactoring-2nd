cbac7dddcb12b3ca069b0a0bdd422b71f42f013e
StrongWise <ghoul300@naver.com>
2024-05-17 17:30:36 +0900

    docs: JSDoc @deprecated

--- src/chap06/change-function-declaration.js
+++ src/chap06/change-function-declaration.js
@@ -1,4 +1,9 @@
 // 함수 이름을 너무 축약한 예
+/**
+ * @deprecated Use `circumference()` instead.
+ * @param radius
+ * @returns {number}
+ */
 export function circum(radius) {
 	return 2 * Math.PI * radius;
 }
