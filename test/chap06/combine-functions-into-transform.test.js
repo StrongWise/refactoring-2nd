40b1bb59c7cff9f41d84beb4e0daf9f2e164be1c
StrongWise <ghoul300@naver.com>
2024-05-20 15:37:29 +0900

    refactor: transform method

--- test/chap06/combine-functions-into-transform.test.js
+++ test/chap06/combine-functions-into-transform.test.js
@@ -1,19 +1,23 @@
 import {expect} from "chai";
 import {acquireReading, baseRate, taxThreshold} from "../../src/chap06/combine-functions-into-transform.js";
+import {enrichReading} from "../../src/chap06/combine-functions-into-transform-origin.js";
 describe('combine-functions-into-transform', () => {
   it('client 1', () => {
-    const aReading = acquireReading()
+    const rawReading = acquireReading();
+    const aReading = enrichReading(rawReading);
     const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
     expect(baseCharge).equal(10000);
   });
   it('client 2', () => {
-    const aReading = acquireReading()
+    const rawReading = acquireReading();
+    const aReading = enrichReading(rawReading);
     const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
     const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
     expect(taxableCharge).equal(9995);
   });
   it('client 3', () => {
-    const aReading = acquireReading()
+    const rawReading = acquireReading();
+    const aReading = enrichReading(rawReading);
     const basicChargeAmount = calculateBaseCharge(aReading);
 
     expect(basicChargeAmount).equal(10000);
