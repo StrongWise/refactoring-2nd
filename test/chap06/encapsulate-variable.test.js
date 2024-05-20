37c6c6f83e1ec49c2bb486048cd73ad3b875e862
StrongWise <ghoul300@naver.com>
2024-05-20 11:11:51 +0900

    refactor: beforeEach data initialize

--- test/chap06/encapsulate-variable.test.js
+++ test/chap06/encapsulate-variable.test.js
@@ -2,14 +2,17 @@ import {expect} from "chai";
 import {defaultOwner, setDefaultOwner} from "../../src/chap06/encapsulate-variable.js";
 
 describe('encapsulate-variable', () => {
+  let spaceship;
+  beforeEach(() => {
+    spaceship = {owner: {}};
+    setDefaultOwner({firstName: '마틴', lastName: '파울러'});
+  })
   it('get spaceship owner', () => {
-    const spaceship = {owner: {}};
     spaceship.owner = defaultOwner();
     expect(spaceship.owner.firstName).equal('마틴');
     expect(spaceship.owner.lastName).equal('파울러');
   });
   it('set spaceship owner', () => {
-    const spaceship = {owner: {}};
     setDefaultOwner({firstName: '레베카', lastName: '파슨스'});
     spaceship.owner = defaultOwner();
     expect(spaceship.owner.firstName).equal('레베카');
