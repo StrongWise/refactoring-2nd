85877d361cc20df0c163dce3cb992b8ab285ad94
StrongWise <ghoul300@naver.com>
2024-05-20 10:48:36 +0900

    feat: encapsulate variable

--- src/chap06/encapsulate-variable.js
+++ src/chap06/encapsulate-variable.js
@@ -1,11 +1,1 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
-
-export function getDefaultOwner() {
-	return defaultOwner;
-}
-
-export function setDefaultOwner(arg) {
-	defaultOwner = arg;
-}
-
-
+export let defaultOwner = { firstName: '마틴', lastName: '파울러' };
