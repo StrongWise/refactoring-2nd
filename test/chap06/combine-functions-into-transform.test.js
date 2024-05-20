c72313c5f1c653ff2e78c3b55b3ac677257a88e4
StrongWise <ghoul300@naver.com>
2024-05-20 15:56:29 +0900

    refactor: inline

--- test/chap06/combine-functions-into-transform.test.js
+++ test/chap06/combine-functions-into-transform.test.js
@@ -1,26 +1,23 @@
 import {expect} from "chai";
 import assert from "node:assert";
 import _ from "lodash";
-import {acquireReading, enrichReading, taxThreshold} from "../../src/chap06/combine-functions-into-transform.js";
+import {acquireReading, enrichReading} from "../../src/chap06/combine-functions-into-transform.js";
 
 describe('combine-functions-into-transform', () => {
   it('client 1', () => {
     const rawReading = acquireReading();
     const aReading = enrichReading(rawReading);
-    const baseCharge = aReading.baseCharge;
-    expect(baseCharge).equal(10000);
+    expect(aReading.baseCharge).equal(10000);
   });
   it('client 2', () => {
     const rawReading = acquireReading();
     const aReading = enrichReading(rawReading);
-    const taxableCharge = aReading.taxableCharge;
-    expect(taxableCharge).equal(9995);
+    expect(aReading.taxableCharge).equal(9995);
   });
   it('client 3', () => {
     const rawReading = acquireReading();
     const aReading = enrichReading(rawReading);
-    const basicChargeAmount = aReading.baseCharge;
-    expect(basicChargeAmount).equal(10000);
+    expect(aReading.baseCharge).equal(10000);
   });
   it('check reading unchanged', () => {
     const baseReading = acquireReading();
