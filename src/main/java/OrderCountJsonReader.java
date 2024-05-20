82b0ec44c2fadef509a6b5b789e105a014d1e11f
StrongWise <ghoul300@naver.com>
2024-05-20 17:58:33 +0900

    refactor: move sout

--- src/main/java/OrderCountJsonReader.java
+++ src/main/java/OrderCountJsonReader.java
@@ -8,25 +8,25 @@ import java.util.stream.Stream;
 public class OrderCountJsonReader {
   public static void main(String[] args) {
     try {
-      run(args);
+      System.out.println(run(args));
     } catch (Exception e) {
       System.err.println(e);
       System.exit(1);
     }
   }
 
-  private static void run(String[] args) throws IOException {
+  static long run(String[] args) throws IOException {
     if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
     String filename = args[args.length - 1];
     File input = Paths.get(filename).toFile();
     ObjectMapper mapper = new ObjectMapper();
     Order[] orders = mapper.readValue(input, Order[].class);
     if (Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
-      System.out.println(Stream.of(orders)
+      return Stream.of(orders)
         .filter(o -> "ready".equals(o.status))
-        .count());
+        .count();
     } else {
-      System.out.println(orders.length);
+      return orders.length;
     }
   }
 }
