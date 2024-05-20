80e16e7c827f869664437ff2e55a26d1bcb4ba2d
StrongWise <ghoul300@naver.com>
2024-05-20 10:56:42 +0900

    refactor: data access function for encapsulate

--- test/chap06/encapsulate-variable.test.js
+++ test/chap06/encapsulate-variable.test.js
@@ -1,18 +1,17 @@
 import {expect} from "chai";
-import {defaultOwner} from "../../src/chap06/encapsulate-variable.js";
+import {getDefaultOwner, setDefaultOwner, defaultOwner} from "../../src/chap06/encapsulate-variable.js";
 
 describe('encapsulate-variable', () => {
   it('get spaceship owner', () => {
     const spaceship = {owner: {}};
-    spaceship.owner = defaultOwner;
+    spaceship.owner = getDefaultOwner();
     expect(spaceship.owner.firstName).equal('마틴');
     expect(spaceship.owner.lastName).equal('파울러');
   });
   it('set spaceship owner', () => {
     const spaceship = {owner: {}};
-    let testDefaultOwner = defaultOwner;
-    testDefaultOwner = {firstName: '레베카', lastName: '파슨스'};
-    spaceship.owner = testDefaultOwner;
+    setDefaultOwner({firstName: '레베카', lastName: '파슨스'});
+    spaceship.owner = getDefaultOwner();
     expect(spaceship.owner.firstName).equal('레베카');
     expect(spaceship.owner.lastName).equal('파슨스');
   });
