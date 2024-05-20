963ddca1abfea6aca7dce70a657408427cb89042
StrongWise <ghoul300@naver.com>
2024-05-20 17:51:54 +0900

    test: add cases

--- src/test/java/OrderCountJsonReaderTest.java
+++ src/test/java/OrderCountJsonReaderTest.java
@@ -6,4 +6,10 @@ public class OrderCountJsonReaderTest {
     @Test public void testEmptyOrders() throws IOException {
       OrderCountJsonReader.main(new String[]{"./src/test/resources/emptyOrders.json"});
     }
+    @Test public void testOrders() throws IOException {
+      OrderCountJsonReader.main(new String[]{"./src/test/resources/orders.json"});
+    }
+    @Test public void testReadyOrders() throws IOException {
+      OrderCountJsonReader.main(new String[]{"-r", "./src/test/resources/readyOrders.json"});
+    }
 }
