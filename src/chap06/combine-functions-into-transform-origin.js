eeb3a710a30a41cdf4c817ea005dd8195d422dc2
StrongWise <ghoul300@naver.com>
2024-05-20 15:32:53 +0900

    feat: combine functions into transform

---
+++ src/chap06/combine-functions-into-transform-origin.js
@@ -0,0 +1,36 @@
+export class Reading {
+	constructor(data) {
+		this._customer = data._customer;
+		this._quantity = data._quantity;
+		this._month = data._month;
+		this._year = data._year;
+	}
+	get customer() {
+		return this._customer;
+	}
+	get quantity() {
+		return this._quantity;
+	}
+	get month() {
+		return this._month;
+	}
+	get year() {
+		return this._year;
+	}
+}
+
+export function acquireReading() {
+  return new Reading({
+    _customer: 'ivan',
+    _quantity: 10,
+    _month: 5,
+    _year: 2024,
+  });
+}
+
+export function baseRate(month, year) {
+  return 1000;
+}
+export function taxThreshold(year) {
+  return 5
+}
