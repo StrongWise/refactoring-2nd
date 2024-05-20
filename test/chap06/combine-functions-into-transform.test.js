188ce871b473db7fa9601030ea8215d1259d7876
StrongWise <ghoul300@naver.com>
2024-05-20 15:53:59 +0900

    refactor: replace method

--- test/chap06/combine-functions-into-transform.test.js
+++ test/chap06/combine-functions-into-transform.test.js
@@ -6,13 +6,13 @@ describe('combine-functions-into-transform', () => {
   it('client 1', () => {
     const rawReading = acquireReading();
     const aReading = enrichReading(rawReading);
-    const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+    const baseCharge = aReading.baseCharge;
     expect(baseCharge).equal(10000);
   });
   it('client 2', () => {
     const rawReading = acquireReading();
     const aReading = enrichReading(rawReading);
-    const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
+    const base = aReading.baseCharge;
     const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
     expect(taxableCharge).equal(9995);
   });
