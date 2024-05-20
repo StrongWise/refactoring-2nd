3915b56e726487ff9bd4d51cde81680ddfc4d961
StrongWise <ghoul300@naver.com>
2024-05-20 10:48:39 +0900

    test: encapsulate variable

--- test/chap06/encapsulate-variable.test.js
+++ test/chap06/encapsulate-variable.test.js
@@ -1,16 +1,19 @@
 import {expect} from "chai";
-import {getDefaultOwner, setDefaultOwner} from "../../src/chap06/encapsulate-variable.js";
+import {defaultOwner} from "../../src/chap06/encapsulate-variable.js";
 
 describe('encapsulate-variable', () => {
-  it('getDefaultOwner', () => {
-    const defaultOwner = getDefaultOwner();
-    expect(defaultOwner.firstName).equal('마틴');
-    expect(defaultOwner.lastName).equal('파울러');
+  it('get spaceship owner', () => {
+    const spaceship = {owner: {}};
+    spaceship.owner = defaultOwner;
+    expect(spaceship.owner.firstName).equal('마틴');
+    expect(spaceship.owner.lastName).equal('파울러');
   });
-  it('setDefaultOwner', () => {
-    setDefaultOwner({firstName: '마틴2', lastName: '파울러2'});
-    const defaultOwner = getDefaultOwner();
-    expect(defaultOwner.firstName).equal('마틴2');
-    expect(defaultOwner.lastName).equal('파울러2');
+  it('set spaceship owner', () => {
+    const spaceship = {owner: {}};
+    let testDefaultOwner = defaultOwner;
+    testDefaultOwner = {firstName: '레베카', lastName: '파슨스'};
+    spaceship.owner = testDefaultOwner;
+    expect(spaceship.owner.firstName).equal('레베카');
+    expect(spaceship.owner.lastName).equal('파슨스');
   });
 });
