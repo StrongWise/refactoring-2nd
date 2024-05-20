9bbd8f85249c614457c7c17463074e5fe652d799
StrongWise <ghoul300@naver.com>
2024-05-20 10:50:04 +0900

    refactor: data access function for encapsulate

--- src/chap06/encapsulate-variable.js
+++ src/chap06/encapsulate-variable.js
@@ -1,1 +1,8 @@
 export let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+export function getDefaultOwner() {
+	return defaultOwner;
+}
+
+export function setDefaultOwner(arg) {
+	defaultOwner = arg;
+}
