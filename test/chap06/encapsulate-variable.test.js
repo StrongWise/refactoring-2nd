79b7ae8c6679db3d72406d92bc6036c9c9cce6dc
StrongWise <ghoul300@naver.com>
2024-05-20 11:02:43 +0900

    refactor: rename

--- test/chap06/encapsulate-variable.test.js
+++ test/chap06/encapsulate-variable.test.js
@@ -1,17 +1,17 @@
 import {expect} from "chai";
-import {getDefaultOwner, setDefaultOwner} from "../../src/chap06/encapsulate-variable.js";
+import {defaultOwner, setDefaultOwner} from "../../src/chap06/encapsulate-variable.js";
 
 describe('encapsulate-variable', () => {
   it('get spaceship owner', () => {
     const spaceship = {owner: {}};
-    spaceship.owner = getDefaultOwner();
+    spaceship.owner = defaultOwner();
     expect(spaceship.owner.firstName).equal('마틴');
     expect(spaceship.owner.lastName).equal('파울러');
   });
   it('set spaceship owner', () => {
     const spaceship = {owner: {}};
     setDefaultOwner({firstName: '레베카', lastName: '파슨스'});
-    spaceship.owner = getDefaultOwner();
+    spaceship.owner = defaultOwner();
     expect(spaceship.owner.firstName).equal('레베카');
     expect(spaceship.owner.lastName).equal('파슨스');
   });
