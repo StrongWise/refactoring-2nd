6f6b1f917fd2681d5df803b03cb9c646a16c5b89
StrongWise <ghoul300@naver.com>
2024-05-21 10:22:35 +0900

    feat: encapsulate record

---
+++ src/chap07/encapsulate-record-origin.js
@@ -0,0 +1,8 @@
+export function simpleRecordEncapsulate(organization, newName) {
+  let result = '';
+
+  result += `<h1>${organization.name}</h1>`; // 읽기 예
+  organization.name = newName; // 쓰기 예
+
+  return result;
+}
