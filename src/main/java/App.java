306e3bda4ff54eb08331295e38f197aaba130ff1
StrongWise <ghoul300@naver.com>
2024-05-21 09:27:10 +0900

    refactor: replace temp with query

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,11 +18,11 @@ public class App {
   static long run(String[] args) throws IOException {
     if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
     CommandLine commandLine = new CommandLine(args);
-    return countOrders(commandLine, args, commandLine.filename());
+    return countOrders(commandLine, args);
   }
 
-  private static long countOrders(CommandLine commandLine, String[] args, String filename) throws IOException {
-    File input = Paths.get(filename).toFile();
+  private static long countOrders(CommandLine commandLine, String[] args) throws IOException {
+    File input = Paths.get(commandLine.filename()).toFile();
     ObjectMapper mapper = new ObjectMapper();
     Order[] orders = mapper.readValue(input, Order[].class);
     if (Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
