1f44c77457b6a0b99c14a436f70dd61860369634
StrongWise <ghoul300@naver.com>
2024-05-21 10:51:03 +0900

    refactor: temp getter

--- src/chap07/encapsulate-record.js
+++ src/chap07/encapsulate-record.js
@@ -1,8 +1,12 @@
 export function simpleRecordEncapsulate(organization, newName) {
   let result = '';
 
-  result += `<h1>${organization.name}</h1>`; // 읽기 예
-  organization.name = newName; // 쓰기 예
-  result += `<h1>${organization.name}</h1>`;
+  result += `<h1>${getRawDataOfOrganization().name}</h1>`; // 읽기 예
+  getRawDataOfOrganization().name = newName; // 쓰기 예
+  result += `<h1>${getRawDataOfOrganization().name}</h1>`;
   return result;
+
+  function getRawDataOfOrganization() {
+    return organization;
+  }
 }
