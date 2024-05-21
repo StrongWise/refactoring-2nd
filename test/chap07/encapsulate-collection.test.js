4978e5218f10b7541c9cf197c9f8caa5711bfcce
StrongWise <ghoul300@naver.com>
2024-05-21 16:20:59 +0900

    refactor: beforeEach test fixture

--- test/chap07/encapsulate-collection.test.js
+++ test/chap07/encapsulate-collection.test.js
@@ -2,36 +2,21 @@ import {Course, Person, readBasicCourseNames} from "../../src/chap07/encapsulate
 import {expect} from "chai";
 
 describe('encapsulate-collection', () => {
+  let aPerson;
+  beforeEach(() => {
+    aPerson = new Person('StrongWise');
+    aPerson.courses = readBasicCourseNames().map(name => new Course(name, false));
+  });
   it('person setter', () => {
-    const aPerson = new Person('StrongWise');
-    const filename = '';
-    let numAdvancedCourses = aPerson.courses.filter(c => c.isAdvanced).length;
-    const basicCourseNames = readBasicCourseNames(filename);
-    aPerson.courses = basicCourseNames.map(name => new Course(name, false));
-    // for (const name of readBasicCourseNames(filename)) {
-    //   aPerson.courses.push(new Course(name, false));
-    // }
-
-    const data = aPerson.courses;
-    expect(data[0].name).to.equal('Korean');
-    expect(data[1].name).to.equal('English');
-    expect(data[2].name).to.equal('Math');
+    expect(aPerson.courses[0].name).to.equal('Korean');
+    expect(aPerson.courses[1].name).to.equal('English');
+    expect(aPerson.courses[2].name).to.equal('Math');
   });
   it('person addCourse', () => {
-    const aPerson = new Person('StrongWise');
-
-    for (const name of readBasicCourseNames()) {
-      aPerson.courses.push(new Course(name, false));
-    }
-
     aPerson.addCourse(new Course('History', false));
     expect(aPerson.courses[3].name).to.equal('History');
   });
   it('person removeCourse', () => {
-    const aPerson = new Person('StrongWise');
-    for (const name of readBasicCourseNames()) {
-      aPerson.courses.push(new Course(name, false));
-    }
     aPerson.removeCourse(aPerson.courses[1], () => {console.log('no')});
     expect(aPerson.courses[0].name).to.equal('Korean');
     expect(aPerson.courses[1].name).to.equal('Math');
