0baeb14fec2a21e8e155da8652e826629d08d068
StrongWise <ghoul300@naver.com>
2024-05-22 10:58:26 +0900

    refactor: more

--- src/chap07/replace-primitive-with-object.js
+++ src/chap07/replace-primitive-with-object.js
@@ -3,6 +3,9 @@ export class Order {
 		this._priority = data.priority;
 	}
 
+  get priority() {
+    return this._priority;
+  }
   get priorityString() {
     return this._priority.toString();
   }
@@ -14,10 +17,35 @@ export class Order {
 
 export class Priority {
   constructor(value) {
-    this._value = value;
+    if (value instanceof Priority) return value;
+    if (Priority.legalValues().includes(value)) {
+      this._value = value;
+    } else {
+      throw new Error(`<${value}>는 유효하지 않은 우선순위입니다.`)
+    }
   }
 
   toString() {
     return this._value;
+  }
+
+  get _index() {
+    return Priority.legalValues().findIndex(s => s === this._value);
+  }
+
+  static legalValues() {
+    return ['low', 'normal', 'high', 'rush'];
+  }
+
+  equals(other) {
+    return this._index === other._index;
+  }
+
+  higherThan(other) {
+    return this._index > other._index;
+  }
+
+  lowerThan(other) {
+    return this._index < other._index;
   }
 }
--- test/chap07/replace-primitive-with-object.test.js
+++ test/chap07/replace-primitive-with-object.test.js
@@ -1,15 +1,15 @@
-import {Order} from "../../src/chap07/replace-primitive-with-object.js";
+import {Order, Priority} from "../../src/chap07/replace-primitive-with-object.js";
 import {expect} from "chai";
 
 describe('replace-primitive-with-object', () => {
   it('Order priority filter', () => {
     const orders = []
-    orders.push(new Order({priority: "high"}));
-    orders.push(new Order({priority: "row"}));
-    orders.push(new Order({priority: "rush"}));
-    orders.push(new Order({priority: "high"}));
+    orders.push(new Order({priority: new Priority("high")}));
+    orders.push(new Order({priority: new Priority("low")}));
+    orders.push(new Order({priority: new Priority("rush")}));
+    orders.push(new Order({priority: new Priority("high")}));
 
-    const highPriorityCount = orders.filter(o => "high" === o.priorityString || "rush" === o.priorityString).length;
+    const highPriorityCount = orders.filter(o => o.priority.higherThan(new Priority("normal"))).length;
     expect(highPriorityCount).to.equal(3);
   });
 });
