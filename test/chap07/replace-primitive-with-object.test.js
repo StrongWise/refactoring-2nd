619b58018febb61baef08b2eff5faafd1c2ad9bd
StrongWise <ghoul300@naver.com>
2024-05-22 09:19:18 +0900

    test: replace-primitive-with-object

---
+++ test/chap07/replace-primitive-with-object.test.js
@@ -0,0 +1,15 @@
+import {Order} from "../../src/chap07/replace-primitive-with-object.js";
+import {expect} from "chai";
+
+describe('replace-primitive-with-object', () => {
+  it('Order priority filter', () => {
+    const orders = []
+    orders.push(new Order({priority: "high"}));
+    orders.push(new Order({priority: "row"}));
+    orders.push(new Order({priority: "rush"}));
+    orders.push(new Order({priority: "high"}));
+
+    const highPriorityCount = orders.filter(o => "high" === o.priority || "rush" === o.priority).length;
+    expect(highPriorityCount).to.equal(3);
+  });
+});
