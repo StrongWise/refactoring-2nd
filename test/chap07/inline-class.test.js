6d93db61f7907ae06e943a5f483b320670292f61
StrongWise <ghoul300@naver.com>
2024-05-23 09:49:16 +0900

    test: inline class

--- test/chap07/inline-class.test.js
+++ test/chap07/inline-class.test.js
@@ -4,15 +4,13 @@ import * as async_hooks from "async_hooks";
 
 describe('inline-class', () => {
   it('check shipment TrackingInformation', () => {
-    const request = {vendor: 'CJ 대한통운'};
+    const request = {vendor: 'CJ 대한통운', trackingNumber: 1234};
 
-    const aShipment = {trackingInformation: new TrackingInformation()}
+    const aShipment = new Shipment();
+    aShipment.trackingInformation = new TrackingInformation();
     aShipment.trackingInformation.shippingCompany = request.vendor;
-    aShipment.trackingInformation.trackingNumber = 1234;
+    aShipment.trackingInformation.trackingNumber = request.trackingNumber;
 
-    const shipment = new Shipment();
-    shipment.trackingInformation = aShipment.trackingInformation;
-
-    expect(shipment.trackingInfo).to.equal('CJ 대한통운: 1234');
+    expect(aShipment.trackingInfo).to.equal('CJ 대한통운: 1234');
   });
 });
