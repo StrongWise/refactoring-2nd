ab7a7add2d0179446e2299f4382f5453f0712c40
StrongWise <ghoul300@naver.com>
2024-05-24 17:22:58 +0900

    refactor: emitPhotoData inline

--- src/chap08/move-statements-to-callers.js
+++ src/chap08/move-statements-to-callers.js
@@ -1,7 +1,8 @@
 export function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
-  emitPhotoData(outStream, person.photo);
+  zztmp(outStream, person.photo);
+  outStream.write(`<p>위치: ${person.photo.location}</p>\n`);
 }
 
 export function listRecentPhotos(outStream, photos) {
@@ -9,7 +10,8 @@ export function listRecentPhotos(outStream, photos) {
     .filter(p => p.date > recentDateCutoff())
     .forEach(p => {
       outStream.write("<div>\n");
-      emitPhotoData(outStream, p);
+      zztmp(outStream, p);
+      outStream.write(`<p>위치: ${p.location}</p>\n`);
       outStream.write("</div>\n");
     });
 }
@@ -17,11 +19,6 @@ export function listRecentPhotos(outStream, photos) {
 function zztmp(outStream, photo) {
   outStream.write(`<p>제목: ${photo.title}</p>\n`);
   outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
-}
-
-function emitPhotoData(outStream, photo) {
-  zztmp(outStream, photo);
-  outStream.write(`<p>위치: ${photo.location}</p>\n`);
 }
 
 function renderPhoto(outStream, aPhoto) {
