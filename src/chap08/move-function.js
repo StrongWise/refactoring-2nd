5db7e07e8b9c20133ecac171020df31bdd55d4f4
StrongWise <ghoul300@naver.com>
2024-05-24 09:50:20 +0900

    refactor: rename & inline

--- src/chap08/move-function.js
+++ src/chap08/move-function.js
@@ -1,15 +1,14 @@
 export function trackSummary(points) {
   const totalTime = calculateTime();
-  const totalDistance = top_calculateDistance(points);
-  const pace = totalTime / 60 / totalDistance;
-  return {time: totalTime, distance: totalDistance, pace: pace};
+  const pace = totalTime / 60 / totalDistance(points);
+  return {time: totalTime, distance: totalDistance(points), pace: pace};
 
   function calculateTime() {
     return 10000;
   }
 }
 
-export function top_calculateDistance(points) {
+export function totalDistance(points) {
   let result = 0;
   for (let i = 1; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);
