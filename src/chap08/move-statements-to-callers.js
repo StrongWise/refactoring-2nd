a3d81a37b14a71040f85a573dd62a4a5f18acbae
StrongWise <ghoul300@naver.com>
2024-05-24 17:20:19 +0900

    refactor: extract zztmp method

--- src/chap08/move-statements-to-callers.js
+++ src/chap08/move-statements-to-callers.js
@@ -1,29 +1,33 @@
 export function renderPerson(outStream, person) {
-    outStream.write(`<p>${person.name}</p>\n`);
-    renderPhoto(outStream, person.photo);
-    emitPhotoData(outStream, person.photo);
+  outStream.write(`<p>${person.name}</p>\n`);
+  renderPhoto(outStream, person.photo);
+  emitPhotoData(outStream, person.photo);
 }
 
 export function listRecentPhotos(outStream, photos) {
-    photos
-        .filter(p => p.date > recentDateCutoff())
-        .forEach(p => {
-            outStream.write("<div>\n");
-            emitPhotoData(outStream, p);
-            outStream.write("</div>\n");
-        });
+  photos
+    .filter(p => p.date > recentDateCutoff())
+    .forEach(p => {
+      outStream.write("<div>\n");
+      emitPhotoData(outStream, p);
+      outStream.write("</div>\n");
+    });
+}
+
+function zztmp(outStream, photo) {
+  outStream.write(`<p>제목: ${photo.title}</p>\n`);
+  outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
 }
 
 function emitPhotoData(outStream, photo) {
-    outStream.write(`<p>제목: ${photo.title}</p>\n`);
-    outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
-    outStream.write(`<p>위치: ${photo.location}</p>\n`);
+  zztmp(outStream, photo);
+  outStream.write(`<p>위치: ${photo.location}</p>\n`);
 }
 
 function renderPhoto(outStream, aPhoto) {
-    outStream.write("");
+  outStream.write("");
 }
 
 function recentDateCutoff() {
-    return new Date().setDate(new Date().getDate() - 7);
+  return new Date().setDate(new Date().getDate() - 7);
 }
