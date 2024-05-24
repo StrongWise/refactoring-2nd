b8445526b69a68fa582cc1385d5dfbd6c1458c7a
StrongWise <ghoul300@naver.com>
2024-05-24 10:27:49 +0900

    feat: move function to another class

--- src/chap08/move-function-origin.js
+++ src/chap08/move-function-origin.js
@@ -31,3 +31,27 @@ export function trackSummary(points) {
     return 10000;
   }
 }
+
+export class Account {
+  constructor(accountType, daysOverdrawn) {
+    this.type = accountType;
+    this.daysOverdrawn = daysOverdrawn;
+  }
+
+  get bankCharge() { // 은행 이자 계산
+    let result = 4.5;
+    if (this.daysOverdrawn > 0) result += this.overdraftCharge;
+    return result;
+  }
+
+  get overdraftCharge() { // 초과 인출 이자 계산
+    if (this.type.isPremium) {
+      const baseCharge = 10;
+      if (this.daysOverdrawn <= 7)
+        return baseCharge;
+      else
+        return baseCharge + (this.daysOverdrawn - 7) * 0.85;
+    } else
+      return this.daysOverdrawn * 1.75;
+  }
+}
--- src/chap08/move-function.js
+++ src/chap08/move-function.js
@@ -33,3 +33,27 @@ function distance(p1, p2) {
 function radians(degrees) {
   return degrees * Math.PI / 180;
 }
+
+export class Account {
+  constructor(accountType, daysOverdrawn) {
+    this.type = accountType;
+    this.daysOverdrawn = daysOverdrawn;
+  }
+
+  get bankCharge() { // 은행 이자 계산
+    let result = 4.5;
+    if (this.daysOverdrawn > 0) result += this.overdraftCharge;
+    return result;
+  }
+
+  get overdraftCharge() { // 초과 인출 이자 계산
+    if (this.type.isPremium) {
+      const baseCharge = 10;
+      if (this.daysOverdrawn <= 7)
+        return baseCharge;
+      else
+        return baseCharge + (this.daysOverdrawn - 7) * 0.85;
+    } else
+      return this.daysOverdrawn * 1.75;
+  }
+}
