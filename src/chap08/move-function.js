c96f111f042c90ff1365037b017b86363d4ca741
StrongWise <ghoul300@naver.com>
2024-05-24 09:53:18 +0900

    chore: hide function

--- src/chap08/move-function.js
+++ src/chap08/move-function.js
@@ -8,7 +8,7 @@ export function trackSummary(points) {
   }
 }
 
-export function totalDistance(points) {
+function totalDistance(points) {
   let result = 0;
   for (let i = 1; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);
