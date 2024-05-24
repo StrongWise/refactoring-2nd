9f1a4e0936418ce775c0f55831e499fbac8eb3df
StrongWise <ghoul300@naver.com>
2024-05-24 14:07:51 +0900

    feat: move field

--- src/chap08/move-field.js
+++ src/chap08/move-field.js
@@ -1,36 +1,59 @@
 export class Customer {
-	constructor(name, discountRate) {
-		this._name = name;
+  constructor(name, discountRate) {
+    this._name = name;
     this._contract = new CustomerContract(dateToday(), discountRate);
-	}
-	get discountRate() {
-		return this._discountRate;
-	}
-  _setDiscountRate(aNumber){
-    this._contract.discountRate = aNumber;
   }
-	becomePreferred() {
-    this._setDiscountRate(this._contract.discountRate + 0.03);
-	}
 
-	applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this._contract.discountRate));
-	}
-}
-
-class CustomerContract {
-	constructor(startDate, discountRate) {
-		this._startDate = startDate;
-    this._discountRate = discountRate;
-	}
   get discountRate() {
     return this._discountRate;
   }
+
+  _setDiscountRate(aNumber) {
+    this._contract.discountRate = aNumber;
+  }
+
+  becomePreferred() {
+    this._setDiscountRate(this._contract.discountRate + 0.03);
+  }
+
+  applyDiscount(amount) {
+    return amount.subtract(amount.multiply(this._contract.discountRate));
+  }
+}
+
+class CustomerContract {
+  constructor(startDate, discountRate) {
+    this._startDate = startDate;
+    this._discountRate = discountRate;
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
+
   set discountRate(value) {
     this._discountRate = value;
   }
 }
 
 function dateToday() {
-	return new Date();
+  return new Date();
+}
+
+export class Account {
+  constructor(number, type, interestRate) {
+    this._number = number;
+    this._type = type;
+    this._interestRate = interestRate;
+  }
+
+  get interestRate() {
+    return this._interestRate;
+  }
+}
+
+export class AccountType {
+  constructor(nameString) {
+    this._name = nameString;
+  }
 }
