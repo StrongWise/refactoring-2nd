b40ae114c2ea1bc217ecb56e8d52974ffa428896
StrongWise <ghoul300@naver.com>
2024-05-24 18:03:27 +0900

    test: slide-statements

--- test/chap08/slide-statements.test.js
+++ test/chap08/slide-statements.test.js
@@ -1,5 +1,6 @@
-import {call} from "../../src/chap08/slide-statements.js";
+import {call, call2} from "../../src/chap08/slide-statements.js";
 import {expect} from "chai";
+import assert from "node:assert";
 
 describe('slide-statements', () => {
   it('should ', () => {
@@ -7,5 +8,10 @@ describe('slide-statements', () => {
     expect(result.charge).to.equal(2530);
     expect(result.discount).to.equal(470);
     expect(result.discountableUnits).to.equal(90);
+  });
+
+  it('should ', () => {
+    assert.deepEqual(call2([], []), {resource: 1});
+    assert.deepEqual(call2([{resource: 1}], [{resource: 1}, {resource: 2}]), {resource: 2});
   });
 });
