f0c455b77d1b1384504b193c42669fe55ffcc317
StrongWise <ghoul300@naver.com>
2024-05-24 14:54:52 +0900

    refactor: inline function

--- src/chap08/move-statements-into-function.js
+++ src/chap08/move-statements-into-function.js
@@ -9,7 +9,8 @@ export function renderPerson(outStream, person) {
 function zznew(p) {
   return [
     `<p>제목: ${p.title}</p>`,
-    emitPhotoData(p)
+    `<p>위치: ${p.location}</p>`,
+    `<p>날짜: ${p.date.toDateString()}</p>`,
   ].join('\n');
 }
 
@@ -22,10 +23,4 @@ export function photoDiv(p) {
 }
 function renderPhoto(p) {
   return `<img src="${p.src}" alt=""/>`;
-}
-function emitPhotoData(aPhoto) {
-  const result = [];
-  result.push(`<p>위치: ${aPhoto.location}</p>`);
-  result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
-  return result.join('\n');
 }
