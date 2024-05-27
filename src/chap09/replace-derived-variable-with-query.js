362571b9a78e0aac6fba8f2861f03dbd79d2b4b0
StrongWise <ghoul300@naver.com>
2024-05-27 16:05:04 +0900

    refactor: make new calculator

--- src/chap09/replace-derived-variable-with-query.js
+++ src/chap09/replace-derived-variable-with-query.js
@@ -1,11 +1,17 @@
+import assert from "node:assert";
 export class ProductionPlan {
 	constructor(data) {
 		this._adjustments = data.adjustments;
 		this._production = data.production;
 	}
 	get production() {
+    assert(this._production === this.calculatedProduction);
 		return this._production;
 	}
+  get calculatedProduction() {
+    return this._adjustments
+      .reduce((sum, a) => sum + a.amount, 0);
+  }
 	applyAdjustment(anAdjustment) {
 		this._adjustments.push(anAdjustment);
 		this._production += anAdjustment.amount;
--- test/chap09/replace-derived-variable-with-query.test.js
+++ test/chap09/replace-derived-variable-with-query.test.js
@@ -3,7 +3,7 @@ import {expect} from "chai";
 
 describe('replace-derived-variable-with-query', () => {
   it('Production', () => {
-    const productionPlan = new ProductionPlan({adjustments: [], production: 10000});
+    const productionPlan = new ProductionPlan({adjustments: [{amount: 10000}], production: 10000});
     expect(productionPlan.production).to.equal(10000);
 
     productionPlan.applyAdjustment({amount: 2000});
