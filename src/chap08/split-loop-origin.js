2be31724e76f4fc642d9ea66d74218a8fdd52e58
StrongWise <ghoul300@naver.com>
2024-05-27 09:35:34 +0900

    fix: rename

--- src/chap08/split-loop-origin.js
+++ src/chap08/split-loop-origin.js
@@ -1,11 +1,9 @@
 export function findYoungestAgeAndCalculateTotalSalary(people) {
-  return `youngestAge: ${(findYoungestAge(people))}, totalSalary: ${(calculateTotalSalary(people))}`;
-}
-
-function calculateTotalSalary(people) {
-  return people.reduce((total, p) => total + p.salary, 0);
-}
-
-function findYoungestAge(people) {
-  return Math.min(...people.map(p => p.age));
+	let youngest = people[0] ? people[0].age : Infinity;
+	let totalSalary = 0;
+	for (const p of people) {
+		if (p.age < youngest) youngest = p.age;
+		totalSalary += p.salary;
+	}
+	return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
 }
