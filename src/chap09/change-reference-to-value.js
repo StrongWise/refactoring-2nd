04f1e8e6d38968537615d26fbf4304ec65b2be84
StrongWise <ghoul300@naver.com>
2024-05-27 17:40:35 +0900

    refactor: change-reference-to-value

--- src/chap09/change-reference-to-value.js
+++ src/chap09/change-reference-to-value.js
@@ -6,27 +6,31 @@ export class Person {
 		return this._telephoneNumber.areaCode;
 	}
 	set officeAreaCode(arg) {
-		this._telephoneNumber.areaCode = arg;
+    this._telephoneNumber = new TelephoneNumber(arg, this.officeNumber);
 	}
 	get officeNumber() {
 		return this._telephoneNumber.number;
 	}
 	set officeNumber(arg) {
-		this._telephoneNumber.number = arg;
+    this._telephoneNumber = new TelephoneNumber(this.officeAreaCode, arg);
 	}
 }
 
 export class TelephoneNumber {
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
+  }
 	get areaCode() {
 		return this._areaCode;
-	}
-	set areaCode(arg) {
-		this._areaCode = arg;
 	}
 	get number() {
 		return this._number;
 	}
-	set number(arg) {
-		this._number = arg;
-	}
+
+  equals(other) {
+    if (!(other instanceof TelephoneNumber)) return false;
+    return this.areaCode === other.areaCode &&
+      this.number === other.number;
+  }
 }
