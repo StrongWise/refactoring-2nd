06e63155646fbbb0174a2ab71625bb41afb0f5f4
StrongWise <ghoul300@naver.com>
2024-05-27 09:30:30 +0900

    refactor: method extract

--- src/chap08/split-loop-origin.js
+++ src/chap08/split-loop-origin.js
@@ -1,12 +1,21 @@
 export function findYoungestAgeAndCalculateTotalSalary(people) {
-	let totalSalary = 0;
-	for (const p of people) {
-		totalSalary += p.salary;
-	}
+  let totalSalary = calculateTotalSalary(people);
+  let youngest = findYoungestAge(people);
+  return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+}
 
-	let youngest = people[0] ? people[0].age : Infinity;
-	for (const p of people) {
-		if (p.age < youngest) youngest = p.age;
-	}
-	return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+function calculateTotalSalary(people) {
+  let totalSalary = 0;
+  for (const p of people) {
+    totalSalary += p.salary;
+  }
+  return totalSalary;
+}
+
+function findYoungestAge(people) {
+  let youngest = people[0] ? people[0].age : Infinity;
+  for (const p of people) {
+    if (p.age < youngest) youngest = p.age;
+  }
+  return youngest;
 }
