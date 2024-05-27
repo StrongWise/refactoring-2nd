65d8dc0df8bc410095fd1b79a02dd10ffc60ea6e
StrongWise <ghoul300@naver.com>
2024-05-27 14:37:29 +0900

    test: split-variable

--- test/chap09/split-variable.test.js
+++ test/chap09/split-variable.test.js
@@ -1,8 +1,8 @@
-import {distanceTravelled} from "../../src/chap09/split-variable.js";
+import {discount, distanceTravelled} from "../../src/chap09/split-variable.js";
 import {expect} from "chai";
 
 describe('split-variable', () => {
-  it('should 82', () => {
+  it('distanceTravelled should 82', () => {
     const scenario = {
       primaryForce: 10,
       secondaryForce: 20,
@@ -10,5 +10,20 @@ describe('split-variable', () => {
       delay: 4
     }
     expect(distanceTravelled(scenario, 10)).to.equal(82);
+  });
+  it('distanceTravelled should 16', () => {
+    const scenario = {
+      primaryForce: 10,
+      secondaryForce: 20,
+      mass: 5,
+      delay: 10
+    }
+    expect(distanceTravelled(scenario, 4)).to.equal(16);
+  });
+
+  it('discount', () => {
+    expect(discount(10, 10)).to.equal(10);
+    expect(discount(100, 10)).to.equal(98);
+    expect(discount(100, 200)).to.equal(97);
   });
 });
