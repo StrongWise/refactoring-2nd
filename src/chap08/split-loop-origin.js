a97e6de3a0deab361ca5407ed2a2bd43ea7dd17e
StrongWise <ghoul300@naver.com>
2024-05-27 09:28:43 +0900

    refactor: split-loop and remove duplicate logic

--- src/chap08/split-loop-origin.js
+++ src/chap08/split-loop-origin.js
@@ -2,8 +2,10 @@ export function findYoungestAgeAndCalculateTotalSalary(people) {
 	let youngest = people[0] ? people[0].age : Infinity;
 	let totalSalary = 0;
 	for (const p of people) {
-		if (p.age < youngest) youngest = p.age;
 		totalSalary += p.salary;
+	}
+	for (const p of people) {
+		if (p.age < youngest) youngest = p.age;
 	}
 	return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
 }
