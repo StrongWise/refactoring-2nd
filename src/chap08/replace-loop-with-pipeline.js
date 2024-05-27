58bc15fda312a9f65c495a68421866db70f393d7
StrongWise <ghoul300@naver.com>
2024-05-27 10:49:02 +0900

    refactor: remove iterate

--- src/chap08/replace-loop-with-pipeline.js
+++ src/chap08/replace-loop-with-pipeline.js
@@ -1,14 +1,9 @@
 export function acquireData(input) {
 	const lines = input.split('\n');
-	const result = [];
-  const loopItems = lines.slice(1)
+	const result = lines.slice(1)
     .filter(line => line.trim() !== "")
     .map(line => line.split(','))
     .filter(record => record[1].trim() === 'India')
     .map(record => ({city: record[0].trim(), phone: record[2].trim()}));
-
-  for (const line of loopItems) {
-    result.push(line);
-	}
 	return result;
 }
