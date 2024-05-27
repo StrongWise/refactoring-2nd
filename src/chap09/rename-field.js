a1ec7f85c0d99f6af7a1c0e470aa7b1724ad0a3b
StrongWise <ghoul300@naver.com>
2024-05-27 15:00:08 +0900

    refactor: make class

--- src/chap09/rename-field.js
+++ src/chap09/rename-field.js
@@ -1,1 +1,24 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+export class Organization {
+  constructor(data) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(aName) {
+    this._name = aName;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  set country(aCountry) {
+    this._country = aCountry;
+  }
+}
+
+const organization = new Organization({name: 'Acme Gooseberries', country: 'GB'});
