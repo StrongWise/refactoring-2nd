f160135ac218172f4d552d1814b0335284b13ea3
StrongWise <ghoul300@naver.com>
2024-05-28 13:54:29 +0900

    refactor: extract condition function

--- src/chap10/consolidate-conditional-expression.js
+++ src/chap10/consolidate-conditional-expression.js
@@ -1,6 +1,9 @@
 export function disabilityAmount(anEmployee) {
-  if ((anEmployee.seniority < 2)
-    || (anEmployee.monthsDisabled > 12)
-    || (anEmployee.isPartTime)) return 0;
+  if (isNotEligibleForDisability()) return 0;
   return 1;
+  function isNotEligibleForDisability() {
+    return (anEmployee.seniority < 2)
+      || (anEmployee.monthsDisabled > 12)
+      || (anEmployee.isPartTime);
+  }
 }
