80e7ab3f2773f2aa0263f9a433375888ab4a3934
StrongWise <ghoul300@naver.com>
2024-05-28 17:27:20 +0900

    refactor: condition merge

--- src/chap10/replace-nested-conditional-with-guard-clauses.js
+++ src/chap10/replace-nested-conditional-with-guard-clauses.js
@@ -14,8 +14,9 @@ function someFinalComputation() {
 
 export function adjustedCapital(anInstrument) {
 	let result = 0;
-	if (anInstrument.capital <= 0) return result;
-  if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return result;
+	if (anInstrument.capital <= 0
+    || anInstrument.interestRate <= 0
+    || anInstrument.duration <= 0) return result;
   result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 	return result;
 }
