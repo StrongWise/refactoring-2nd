eb87550cf97deb381e769d0de71f2d118ad211bd
StrongWise <ghoul300@naver.com>
2024-05-28 13:53:14 +0900

    refactor: condition merge

--- src/chap10/consolidate-conditional-expression.js
+++ src/chap10/consolidate-conditional-expression.js
@@ -1,6 +1,6 @@
 export function disabilityAmount(anEmployee) {
   if ((anEmployee.seniority < 2)
-    || (anEmployee.monthsDisabled > 12)) return 0;
-	if (anEmployee.isPartTime) return 0;
+    || (anEmployee.monthsDisabled > 12)
+    || (anEmployee.isPartTime)) return 0;
   return 1;
 }
