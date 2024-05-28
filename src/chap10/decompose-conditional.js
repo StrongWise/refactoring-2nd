04353c4b450741b77fa93a28eabdf7727982bfac
StrongWise <ghoul300@naver.com>
2024-05-28 13:39:01 +0900

    refactor: extract function

--- src/chap10/decompose-conditional.js
+++ src/chap10/decompose-conditional.js
@@ -1,10 +1,19 @@
 export function charge(aDate, plan, quantity) {
   let charge;
+
   if (summer())
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
+    charge = summerCharge();
+  else
+    charge = regularCharge();
+
   return charge;
 
+  function regularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+  }
+  function summerCharge() {
+    return quantity * plan.summerRate;
+  }
   function summer() {
     return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
   }
