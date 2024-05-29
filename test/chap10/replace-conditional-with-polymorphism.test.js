a45c147dbe3abd0c0e24d59f2c13d1c96337b03b
StrongWise <ghoul300@naver.com>
2024-05-29 10:09:52 +0900

    test: replace-conditional-with-polymorphism

---
+++ test/chap10/replace-conditional-with-polymorphism.test.js
@@ -0,0 +1,69 @@
+import {plumages, speeds} from "../../src/chap10/replace-conditional-with-polymorphism.js";
+import {expect} from "chai";
+
+describe('replace-conditional-with-polymorphism', () => {
+  const birds = [
+    {
+      name: '기본 유럽 제비',
+      type: '유럽 제비',
+      numberOfCoconuts: 0,
+      voltage: 100,
+      isNailed: false
+    },
+    {
+      name: '보통 아프리카 제비',
+      type: '아프리카 제비',
+      numberOfCoconuts: 0,
+      voltage: 100,
+      isNailed: false
+    },
+    {
+      name: '지친 아프리카 제비',
+      type: '아프리카 제비',
+      numberOfCoconuts: 4,
+      voltage: 100,
+      isNailed: false
+    },
+    {
+      name: '예쁜 노르웨이 파랑 앵무',
+      type: '노르웨이 파랑 앵무',
+      numberOfCoconuts: 0,
+      voltage: 90,
+      isNailed: false
+    },
+    {
+      name: '그을린 노르웨이 파랑 앵무',
+      type: '노르웨이 파랑 앵무',
+      numberOfCoconuts: 0,
+      voltage: 400,
+      isNailed: false
+    },
+    {
+      name: '까치',
+      type: '까치',
+      numberOfCoconuts: 0,
+      voltage: 100,
+      isNailed: false
+    }
+  ];
+  it('plumages', () => {
+    const plumageMap = plumages(birds);
+    console.log(plumageMap);
+    expect(plumageMap.get('기본 유럽 제비')).to.equal('보통이다');
+    expect(plumageMap.get('보통 아프리카 제비')).to.equal('보통이다');
+    expect(plumageMap.get('지친 아프리카 제비')).to.equal('지쳤다');
+    expect(plumageMap.get('예쁜 노르웨이 파랑 앵무')).to.equal('예쁘다');
+    expect(plumageMap.get('그을린 노르웨이 파랑 앵무')).to.equal('그을렸다');
+    expect(plumageMap.get('까치')).to.equal('알 수 없다');
+  });
+  it('speeds', () => {
+    const speedMap = speeds(birds);
+    console.log(speedMap);
+    expect(speedMap.get('기본 유럽 제비')).to.equal(35);
+    expect(speedMap.get('보통 아프리카 제비')).to.equal(40);
+    expect(speedMap.get('지친 아프리카 제비')).to.equal(32);
+    expect(speedMap.get('예쁜 노르웨이 파랑 앵무')).to.equal(19);
+    expect(speedMap.get('그을린 노르웨이 파랑 앵무')).to.equal(50);
+    expect(speedMap.get('까치')).null;
+  });
+});
