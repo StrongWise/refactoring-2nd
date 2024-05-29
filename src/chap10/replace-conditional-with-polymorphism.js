610eda25cc42af0b7a3c76be4450dd2b97c6e7a6
StrongWise <ghoul300@naver.com>
2024-05-29 10:29:51 +0900

    refactor: move logic to subclass

--- src/chap10/replace-conditional-with-polymorphism.js
+++ src/chap10/replace-conditional-with-polymorphism.js
@@ -22,7 +22,7 @@ export class Bird {
   get plumage() {
     switch (this.type) {
       case '유럽 제비':
-        return '보통이다';
+        throw '오류 발생';
       case '아프리카 제비':
         return this.numberOfCoconuts > 2 ? '지쳤다' : '보통이다';
       case '노르웨이 파랑 앵무':
@@ -59,11 +59,28 @@ function createBird(bird) {
   }
 }
 
-class EuropeanSwallow extends Bird{
+class EuropeanSwallow extends Bird {
+  get plumage() {
+    return '보통이다';
+  }
+
 }
 
-class AfricanSwallow extends Bird{
+class AfricanSwallow extends Bird {
 }
 
-class NorwegianBlueParrot extends Bird{
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    switch (this.type) {
+      case '유럽 제비':
+        return '보통이다';
+      case '아프리카 제비':
+        return this.numberOfCoconuts > 2 ? '지쳤다' : '보통이다';
+      case '노르웨이 파랑 앵무':
+        return this.voltage > 100 ? '그을렸다' : '예쁘다';
+      default:
+        return '알 수 없다';
+    }
+  }
+
 }
