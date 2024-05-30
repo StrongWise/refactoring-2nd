23564729f4e4d903156ed9433931d9c76b95fda7
StrongWise <ghoul300@naver.com>
2024-05-30 16:59:07 +0900

    refactor: remove side effects

--- src/chap11/separate-query-from-modifier.js
+++ src/chap11/separate-query-from-modifier.js
@@ -15,11 +15,9 @@ export function alertForMiscreant(people) {
 export function findMiscreant(people) {
 	for (const p of people) {
 		if (p === '조커') {
-			setOffAlarms();
 			return '조커';
 		}
 		if (p === '사루만') {
-			setOffAlarms();
 			return '사루만';
 		}
 	}
