fdb76096b4e81202ab2d23a890e7c7fe657688b4
StrongWise <ghoul300@naver.com>
2024-05-30 17:02:04 +0900

    refactor: remove duplicate code

--- src/chap11/separate-query-from-modifier.js
+++ src/chap11/separate-query-from-modifier.js
@@ -1,15 +1,5 @@
 export function alertForMiscreant(people) {
-	for (const p of people) {
-		if (p === '조커') {
-			setOffAlarms();
-			return;
-		}
-		if (p === '사루만') {
-			setOffAlarms();
-			return;
-		}
-	}
-	return;
+  if (findMiscreant(people) !== '') setOffAlarms();
 }
 
 export function findMiscreant(people) {
