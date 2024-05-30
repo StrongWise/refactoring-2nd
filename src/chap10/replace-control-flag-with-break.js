908354538a2b9ba7d919ee42ff4a895529c185c9
StrongWise <ghoul300@naver.com>
2024-05-30 15:50:45 +0900

    refactor: remove flag

--- src/chap10/replace-control-flag-with-break.js
+++ src/chap10/replace-control-flag-with-break.js
@@ -1,18 +1,15 @@
 function checkForMiscreants(people) {
-  let found = false;
   for (const p of people) {
-    if (!found) {
-      if (p === '조커') {
-        sendAlert(p);
-        return true;
-      }
-      if (p === '사루만') {
-        sendAlert(p);
-        return true;
-      }
+    if (p === '조커') {
+      sendAlert(p);
+      return true;
+    }
+    if (p === '사루만') {
+      sendAlert(p);
+      return true;
     }
   }
-  return found;
+  return false;
 }
 
 export function findMiscreant(people) {
