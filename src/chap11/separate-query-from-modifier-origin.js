b19888e7e026c6a430c8697e2828a1aceb29a8f6
StrongWise <ghoul300@naver.com>
2024-05-30 16:54:45 +0900

    feat: separate-query-from-modifier

---
+++ src/chap11/separate-query-from-modifier-origin.js
@@ -0,0 +1,17 @@
+export function alertForMiscreant(people) {
+	for (const p of people) {
+		if (p === '조커') {
+			setOffAlarms();
+			return '조커';
+		}
+		if (p === '사루만') {
+			setOffAlarms();
+			return '사루만';
+		}
+	}
+	return '';
+}
+
+function setOffAlarms() {
+	console.log('setOffAlarms');
+}
