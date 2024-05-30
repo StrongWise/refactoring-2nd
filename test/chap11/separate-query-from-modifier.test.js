7b6f028d01eee3ff916423929f940d1ac9e8c68a
StrongWise <ghoul300@naver.com>
2024-05-30 17:01:05 +0900

    refactor: change caller

--- test/chap11/separate-query-from-modifier.test.js
+++ test/chap11/separate-query-from-modifier.test.js
@@ -1,10 +1,16 @@
-import {alertForMiscreant} from "../../src/chap11/separate-query-from-modifier.js";
+import {alertForMiscreant, findMiscreant} from "../../src/chap11/separate-query-from-modifier.js";
 import {expect} from "chai";
 
 describe('separate-query-from-modifier', () => {
   it('alertForMiscreant', () => {
-    expect(alertForMiscreant([])).empty;
-    expect(alertForMiscreant(['','조커',''])).to.equal('조커');
-    expect(alertForMiscreant(['사루만'])).to.equal('사루만');
+    const people = [];
+    expect(findMiscreant(people)).empty;
+    alertForMiscreant(people);
+    const people1 = ['','조커',''];
+    expect(findMiscreant(people1)).to.equal('조커');
+    alertForMiscreant(people1);
+    const people2 = ['사루만'];
+    expect(findMiscreant(people2)).to.equal('사루만');
+    alertForMiscreant(people2);
   });
 });
