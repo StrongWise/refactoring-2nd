28da601cc87ce350292b0fc0b695a05d3d3b1d12
StrongWise <ghoul300@naver.com>
2024-06-03 14:41:44 +0900

    refactor: method extract

--- test/chap11/preserve-whole-object.test.js
+++ test/chap11/preserve-whole-object.test.js
@@ -1,6 +1,12 @@
 import {HeatingPlan} from "../../src/chap11/preserve-whole-object.js";
 import {expect} from "chai";
 
+function xxNEWwithinRange(aPlan, tempRange) {
+  const low = tempRange.low;
+  const high = tempRange.high;
+  return aPlan.withinRange2(low, high);
+}
+
 describe('preserve-whole-object', () => {
   it('HeatingPlan', () => {
     const aPlan = new HeatingPlan(10, 20);
@@ -14,16 +20,12 @@ describe('preserve-whole-object', () => {
     const aPlan = new HeatingPlan(10, 20);
     let aRoom = {daysTempRange: {low: 10, high: 20}}
     let tempRange = aRoom.daysTempRange;
-    let low = tempRange.low;
-    let high = tempRange.high;
-    let isWithinRange = aPlan.withinRange2(low, high);
+    let isWithinRange = xxNEWwithinRange(aPlan, tempRange);
     expect(isWithinRange).to.equal(true);
 
     aRoom = {daysTempRange: {low: 5, high: 20}}
     tempRange = aRoom.daysTempRange;
-    low = tempRange.low;
-    high = tempRange.high;
-    isWithinRange = aPlan.withinRange2(low, high);
+    isWithinRange = xxNEWwithinRange(aPlan, tempRange);
     expect(isWithinRange).to.equal(false);
   });
 });
