44308019350a2a118a5db38cc45f7dae1e4669f4
StrongWise <ghoul300@naver.com>
2024-06-03 14:45:58 +0900

    refactor: move function

--- src/chap11/preserve-whole-object.js
+++ src/chap11/preserve-whole-object.js
@@ -1,15 +1,25 @@
 export class HeatingPlan {
-  _temperatureRange= {low:0, high:0};
-	constructor(low, high) {
-		this._temperatureRange.low = low;
-		this._temperatureRange.high = high;
+  _temperatureRange = {low: 0, high: 0};
+
+  constructor(low, high) {
+    this._temperatureRange.low = low;
+    this._temperatureRange.high = high;
   }
+
   withinRange(aNumberRange) {
     return (aNumberRange.low >= this._temperatureRange.low)
       && (aNumberRange.high <= this._temperatureRange.high);
-	}
-	withinRange2(bottom, top) {
-		return (bottom >= this._temperatureRange.low)
+  }
+
+  withinRange2(bottom, top) {
+    return (bottom >= this._temperatureRange.low)
       && (top <= this._temperatureRange.high);
-	}
+  }
+
+  xxNEWwithinRange(tempRange) {
+    const low = tempRange.low;
+    const high = tempRange.high;
+    return this.withinRange2(low, high);
+  }
+
 }
--- test/chap11/preserve-whole-object.test.js
+++ test/chap11/preserve-whole-object.test.js
@@ -1,12 +1,6 @@
 import {HeatingPlan} from "../../src/chap11/preserve-whole-object.js";
 import {expect} from "chai";
 
-function xxNEWwithinRange(aPlan, tempRange) {
-  const low = tempRange.low;
-  const high = tempRange.high;
-  return aPlan.withinRange2(low, high);
-}
-
 describe('preserve-whole-object', () => {
   it('HeatingPlan', () => {
     const aPlan = new HeatingPlan(10, 20);
@@ -20,12 +14,12 @@ describe('preserve-whole-object', () => {
     const aPlan = new HeatingPlan(10, 20);
     let aRoom = {daysTempRange: {low: 10, high: 20}}
     let tempRange = aRoom.daysTempRange;
-    let isWithinRange = xxNEWwithinRange(aPlan, tempRange);
+    let isWithinRange = aPlan.xxNEWwithinRange(tempRange);
     expect(isWithinRange).to.equal(true);
 
     aRoom = {daysTempRange: {low: 5, high: 20}}
     tempRange = aRoom.daysTempRange;
-    isWithinRange = xxNEWwithinRange(aPlan, tempRange);
+    isWithinRange = aPlan.xxNEWwithinRange(tempRange);
     expect(isWithinRange).to.equal(false);
   });
 });
