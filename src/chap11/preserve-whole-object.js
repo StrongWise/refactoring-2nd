affab297f0b49796fa5debe5ddf553958e7f5f8c
StrongWise <ghoul300@naver.com>
2024-06-03 14:22:09 +0900

    feat: preserve-whole-object

---
+++ src/chap11/preserve-whole-object.js
@@ -0,0 +1,12 @@
+export class HeatingPlan {
+  _temperatureRange= {low:0, high:0};
+	constructor(low, high) {
+		this._temperatureRange.low = low;
+		this._temperatureRange.high = high;
+	}
+
+	withinRange(bottom, top) {
+		return (bottom >= this._temperatureRange.low)
+      && (top <= this._temperatureRange.high);
+	}
+}
