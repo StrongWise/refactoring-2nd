78566344134a4cf768ef397659be13f8d63c584b
StrongWise <ghoul300@naver.com>
2024-06-03 13:12:07 +0900

    test: remove-flag-argument

--- test/chap11/remove-flag-argument.test.js
+++ test/chap11/remove-flag-argument.test.js
@@ -1,6 +1,6 @@
 import {
   bookConcert,
-  premiumBookConcert, regularDeliveryDate,
+  premiumBookConcert, regularDeliveryDate, deliveryDate,
   rushDeliveryDate
 } from "../../src/chap11/remove-flag-argument.js";
 import {expect} from "chai";
@@ -38,6 +38,33 @@ describe('remove-flag-argument', () => {
     }
     expect(rushDeliveryDate(NHOrder).toFormat('yyyy-MM-dd')).to.equal('2024-06-04');
     expect(regularDeliveryDate(NHOrder).toFormat('yyyy-MM-dd')).to.equal('2024-06-06');
+  });
+
+  it('Delivery date 2', () => {
+    let MAOrder = {
+      deliveryState:'MA',
+      placedOn: DateTime.fromISO('2024-06-01')
+    }
+    expect(deliveryDate(MAOrder, true).toFormat('yyyy-MM-dd')).to.equal('2024-06-03');
+    expect(deliveryDate(MAOrder, false).toFormat('yyyy-MM-dd')).to.equal('2024-06-05');
+    let NYOrder = {
+      deliveryState:'NY',
+      placedOn: DateTime.fromISO('2024-06-01')
+    }
+    expect(deliveryDate(NYOrder, true).toFormat('yyyy-MM-dd')).to.equal('2024-06-04');
+    expect(deliveryDate(NYOrder, false).toFormat('yyyy-MM-dd')).to.equal('2024-06-05');
+    let MEOrder = {
+      deliveryState:'ME',
+      placedOn: DateTime.fromISO('2024-06-01')
+    }
+    expect(deliveryDate(MEOrder, true).toFormat('yyyy-MM-dd')).to.equal('2024-06-05');
+    expect(deliveryDate(MEOrder, false).toFormat('yyyy-MM-dd')).to.equal('2024-06-06');
+    let NHOrder = {
+      deliveryState:'NH',
+      placedOn: DateTime.fromISO('2024-06-01')
+    }
+    expect(deliveryDate(NHOrder, true).toFormat('yyyy-MM-dd')).to.equal('2024-06-04');
+    expect(deliveryDate(NHOrder, false).toFormat('yyyy-MM-dd')).to.equal('2024-06-06');
   });
 });
 
