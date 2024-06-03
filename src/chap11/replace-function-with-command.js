d9ca36ed1d6b5908c54832241143c37cab3c8e5a
StrongWise <ghoul300@naver.com>
2024-06-03 17:56:09 +0900

    refactor: local variable to field

--- src/chap11/replace-function-with-command.js
+++ src/chap11/replace-function-with-command.js
@@ -10,12 +10,12 @@ class Scorer {
   }
   execute() {
     this._result = 0;
-    let healthLevel = 0;
-    let highMedicalRiskFlag = false;
+    this._healthLevel = 0;
+    this._highMedicalRiskFlag = false;
 
     if (this._medicalExam.isSmoker) {
-      healthLevel += 10;
-      highMedicalRiskFlag = true;
+      this._healthLevel += 10;
+      this._highMedicalRiskFlag = true;
     }
 
     let certificationGrade = 'regular';
@@ -23,7 +23,7 @@ class Scorer {
       certificationGrade = 'low';
       this._result -= 5;
     }
-    this._result -= Math.max(healthLevel - 5, 0);
+    this._result -= Math.max(this._healthLevel - 5, 0);
     return this._result;
   }
 }
