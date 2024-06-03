69939ce23c483d51c84bf6c4d51811d2c3ee8452
StrongWise <ghoul300@naver.com>
2024-06-03 16:38:59 +0900

    fix: Object to class

--- test/chap11/replace-query-with-parameter.test.js
+++ test/chap11/replace-query-with-parameter.test.js
@@ -7,9 +7,9 @@ describe('replace-query-with-parameter', () => {
     thermostat.currentTemperature = 20;
   })
   it('setAirConditioner', () => {
-    expect(setAirConditioner({targetTemperature: 19})).to.equal('setToCool');
-    expect(setAirConditioner({targetTemperature: 20})).to.equal('setOff');
-    expect(setAirConditioner({targetTemperature: 21})).to.equal('setToHeat');
+    expect(setAirConditioner(new HeatingPlan(16, 19))).to.equal('setToCool');
+    expect(setAirConditioner(new HeatingPlan(17, 20))).to.equal('setOff');
+    expect(setAirConditioner(new HeatingPlan(27, 29))).to.equal('setToHeat');
   });
 
   it('HeatingPlan', () => {
