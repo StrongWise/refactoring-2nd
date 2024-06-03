f980ec62c19c175ccdf9ecb164360a6cec533bc1
StrongWise <ghoul300@naver.com>
2024-06-03 14:22:33 +0900

    test: preserve-whole-object

--- test/chap11/preserve-whole-object.test.js
+++ test/chap11/preserve-whole-object.test.js
@@ -2,7 +2,7 @@ import {HeatingPlan} from "../../src/chap11/preserve-whole-object.js";
 import {expect} from "chai";
 
 describe('preserve-whole-object', () => {
-  it('', () => {
+  it('HeatingPlan', () => {
     const aPlan = new HeatingPlan(10, 20);
     let aRoom = {daysTempRange: {low: 10, high: 20}}
     let low = aRoom.daysTempRange.low;
