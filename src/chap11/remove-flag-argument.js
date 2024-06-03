cb2652b4af2718f3fac3710c3246855056cfd5e0
StrongWise <ghoul300@naver.com>
2024-06-03 13:12:03 +0900

    feat: remove-flag-argument

--- src/chap11/remove-flag-argument.js
+++ src/chap11/remove-flag-argument.js
@@ -21,3 +21,25 @@ export function regularDeliveryDate(anOrder) {
   else deliveryTime = 4;
   return anOrder.placedOn.plus({days: 2 + deliveryTime});
 }
+
+export function deliveryDate(anOrder, isRush) {
+  let result;
+  let deliveryTime;
+  if (anOrder.deliveryState === "MA" || anOrder.deliveryState === "CT") {
+    deliveryTime = isRush ? 1 : 2;
+  } else if (anOrder.deliveryState === "NY" || anOrder.deliveryState === "NH") {
+    deliveryTime = 2;
+    if (anOrder.deliveryState === "NH" && !isRush) {
+      deliveryTime = 3;
+    }
+  } else if (isRush) {
+    deliveryTime = 3;
+  } else if (anOrder.deliveryState === "ME") {
+    deliveryTime = 3;
+  } else {
+    deliveryTime = 4;
+  }
+  result = anOrder.placedOn.plus({days: 2 + deliveryTime});
+  if (isRush) result = result.minus({days: 1});
+  return result;
+}
