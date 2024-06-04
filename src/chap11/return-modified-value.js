8a7d12263a68b6f5f5ffb799ea6391e28330fbe8
StrongWise <ghoul300@naver.com>
2024-06-04 13:50:47 +0900

    refactor: rename

--- src/chap11/return-modified-value.js
+++ src/chap11/return-modified-value.js
@@ -1,6 +1,6 @@
 const points = [{ elevation: 1 },{ elevation: 2 },{ elevation: 10 }];
 
-export let totalAscent = 0;
+let totalAscent = 0;
 let totalTime = 0;
 let totalDistance = 0;
 
@@ -9,11 +9,12 @@ calculateTime();
 calculateDistance();
 const pace = totalTime / 60 / totalDistance;
 export function calculateAscent() {
+  let result = 0;
 	for (let i = 1; i < points.length; i++) {
 		const verticalChange = points[i].elevation - points[i - 1].elevation;
-		totalAscent += verticalChange > 0 ? verticalChange : 0;
+		result += verticalChange > 0 ? verticalChange : 0;
 	}
-  return totalAscent;
+  return result;
 }
 
 function calculateTime() {
--- test/chap11/return-modified-value.test.js
+++ test/chap11/return-modified-value.test.js
@@ -1,10 +1,8 @@
-import {calculateAscent, totalAscent} from "../../src/chap11/return-modified-value.js";
+import {calculateAscent} from "../../src/chap11/return-modified-value.js";
 import {expect} from "chai";
 
 describe('return-modified-value', () => {
   it('calculate Ascent', () => {
-    expect(totalAscent).to.equal(9);
-    calculateAscent();
-    expect(totalAscent).to.equal(18);
+    expect(calculateAscent()).to.equal(9);
   });
 });
