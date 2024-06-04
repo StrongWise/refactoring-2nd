916cb5bd93d2a126027884ae94c1a1e86ac0b52b
StrongWise <ghoul300@naver.com>
2024-06-04 17:31:38 +0900

    refactor: trap method > Must be implemented in subclasses

--- src/chap12/pull-up-method.js
+++ src/chap12/pull-up-method.js
@@ -1,15 +1,31 @@
 export class Party {
-	constructor() {
-		this.monthlyCost = 1;
-	}
-	get annualCost() {
-		return this.monthlyCost * 12;
-	}
+  constructor() {
+    this._monthlyCost = 1;
+  }
+
+  get monthlyCost() {
+    throw new SubClassResponsibilityError();
+  }
+
+  get annualCost() {
+    return this._monthlyCost * 12;
+  }
 }
 
 export class Employee extends Party {
+  get monthlyCost() {
+    return this._monthlyCost
+  }
 }
 
 export class Department extends Party {
+  get monthlyCost() {
+    return this._monthlyCost
+  }
 }
 
+class SubClassResponsibilityError extends Error{
+  constructor(errorCode) {
+    super(`Sub Class Responsibility : ${errorCode}`);
+  }
+}
--- test/chap12/pull-up-method.test.js
+++ test/chap12/pull-up-method.test.js
@@ -5,6 +5,8 @@ describe('pull-up-method', () => {
   it('', () => {
     expect(new Department().annualCost).to.equal(12);
     expect(new Employee().annualCost).to.equal(12);
-    expect(new Party().monthlyCost).to.equal(1);
+    expect(new Department().monthlyCost).to.equal(1);
+    expect(new Employee().monthlyCost).to.equal(1);
+    // expect(new Party().monthlyCost).to.equal(1);
   });
 });
