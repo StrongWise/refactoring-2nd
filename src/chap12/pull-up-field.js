7df79679ece58a30f4bfeb470fa3161b4ea82477
StrongWise <ghoul300@naver.com>
2024-06-04 17:44:18 +0900

    refactor: pull-up-field

--- src/chap12/pull-up-field.js
+++ src/chap12/pull-up-field.js
@@ -1,26 +1,22 @@
 export class Employee {
   constructor(data) {
     this._id = data.id
+    this._name = data.name;
+  }
+
+  get name() {
+    return this._name;
   }
 }
 
 export class Salesperson extends Employee {
   constructor(data) {
     super(data);
-    this._name = data.name;
-  }
-
-  get name() {
-    return this._name;
   }
 }
 export class Engineer extends Employee {
   constructor(data) {
     super(data);
-    this._name = data.name;
   }
 
-  get name() {
-    return this._name;
-  }
 }
