9df1ec2d73d61e251413f9721a42cb50f2b00da6
StrongWise <ghoul300@naver.com>
2024-06-04 10:50:27 +0900

    refactor: inline & remove dead class

--- src/chap11/replace-command-with-function.js
+++ src/chap11/replace-command-with-function.js
@@ -1,14 +1,4 @@
-export class ChargeCalculator {
-	constructor(customer, usage, provider) {
-	}
-
-	charge(customer, usage, provider) {
-    const baseCharge = customer.baseRate * usage
-		return baseCharge + provider.connectionCharge;
-	}
-}
-
 export function charge(customer, usage, provider) {
-	return new ChargeCalculator(customer, usage, provider)
-    .charge(customer, usage, provider);
+  const baseCharge = customer.baseRate * usage
+  return baseCharge + provider.connectionCharge;
 }
