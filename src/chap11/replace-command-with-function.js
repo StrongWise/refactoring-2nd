ccc7e539a6ef22ab37bc30417d986fc0073de10a
StrongWise <ghoul300@naver.com>
2024-06-04 10:47:35 +0900

    refactor: change function declaration

--- src/chap11/replace-command-with-function.js
+++ src/chap11/replace-command-with-function.js
@@ -5,12 +5,13 @@ export class ChargeCalculator {
 		this._provider = provider;
 	}
 
-	get charge() {
+	charge(customer, usage, provider) {
     const baseCharge = this._customer.baseRate * this._usage
 		return baseCharge + this._provider.connectionCharge;
 	}
 }
 
 export function charge(customer, usage, provider) {
-	return new ChargeCalculator(customer, usage, provider).charge;
+	return new ChargeCalculator(customer, usage, provider)
+    .charge(customer, usage, provider);
 }
