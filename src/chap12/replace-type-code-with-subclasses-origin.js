e60f0a3164d8d18237a8097927e914db84c3df3b
StrongWise <ghoul300@naver.com>
2024-06-05 11:43:07 +0900

    feat: replace-type-code-with-subclasses

--- src/chap12/replace-type-code-with-subclasses-origin.js
+++ src/chap12/replace-type-code-with-subclasses-origin.js
@@ -14,3 +14,26 @@ export class Employee {
     return `${this._name} (${this._type})`;
   }
 }
+
+export class Employee2 {
+  constructor(name, type) {
+    this.validateType(type);
+    this._name = name;
+    this._type = type;
+  }
+
+  validateType(arg) {
+    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
+      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+  }
+  get type() {return this._type;}
+  set type(arg) {this._type = arg;}
+
+  get capitalizedType() {
+    return this._type.charAt(0).toUpperCase() + this._type.substr(1).toLowerCase();
+  }
+  toString() {
+    return `${this._name} (${this._type})`;
+  }
+}
--- src/chap12/replace-type-code-with-subclasses.js
+++ src/chap12/replace-type-code-with-subclasses.js
@@ -27,3 +27,26 @@ export function createEmployee(name, type) {
     default: throw new Error(`${type}라는 직원 유형은 없습니다.`);
   }
 }
+
+export class Employee2 {
+  constructor(name, type) {
+    this.validateType(type);
+    this._name = name;
+    this._type = type;
+  }
+
+  validateType(arg) {
+    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
+      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+  }
+  get type() {return this._type;}
+  set type(arg) {this._type = arg;}
+
+  get capitalizedType() {
+    return this._type.charAt(0).toUpperCase() + this._type.substr(1).toLowerCase();
+  }
+  toString() {
+    return `${this._name} (${this._type})`;
+  }
+}
