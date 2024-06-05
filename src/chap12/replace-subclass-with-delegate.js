9dc83ad3c0a4c35e281ce13ac4fbe88666b22f85
StrongWise <ghoul300@naver.com>
2024-06-05 16:41:55 +0900

    refactor: move to delegate class

--- src/chap12/replace-subclass-with-delegate.js
+++ src/chap12/replace-subclass-with-delegate.js
@@ -11,9 +11,11 @@ export class Booking {
   }
 
   get basePrice() {
+    let result = this._show.price;
+    if (this.isPeakDay) result += Math.round(result * 0.15);
     return (this._premiumDelegate)
-      ? this._premiumDelegate.basePrice
-      : this._privateBasePrice;
+      ? this._premiumDelegate.extendBasePrice(result)
+      : result;
   }
 
   get _privateBasePrice() {
@@ -65,5 +67,8 @@ export class PremiumBookingDelegate {
 
   get basePrice() {
     return Math.round(this._host._privateBasePrice + this._extras.premiumFee);
+  }
+  extendBasePrice(base) {
+    return Math.round(base + this._extras.premiumFee);
   }
 }
