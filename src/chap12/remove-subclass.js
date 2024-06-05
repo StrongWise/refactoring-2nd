63623d44db02bc1cb9c38181ed929315e6a4e3c3
StrongWise <ghoul300@naver.com>
2024-06-05 13:48:49 +0900

    feat: load from input method

--- src/chap12/remove-subclass.js
+++ src/chap12/remove-subclass.js
@@ -23,3 +23,19 @@ export function createMale(name) {
 export function createFemale(name) {
   return new Female(name);
 }
+
+export function loadFromInput(data) {
+  const result = [];
+  data.forEach(aRecord => {
+    let p;
+    switch (aRecord.gender) {
+      case 'M': p = new Male(aRecord.name);
+        break;
+      case 'F': p = new Female(aRecord.name);
+        break;
+      default: p = new Person(aRecord.name);
+    }
+    result.push(p);
+  })
+  return result;
+}
--- test/chap12/remove-subclass.test.js
+++ test/chap12/remove-subclass.test.js
@@ -1,9 +1,28 @@
-import {Male, createMale, createFemale} from "../../src/chap12/remove-subclass.js";
+import {
+  Male,
+  createMale,
+  createFemale,
+  loadFromInput,
+  Person,
+  Female
+} from "../../src/chap12/remove-subclass.js";
 import {expect} from "chai";
 describe('remove-subclass', () => {
   it('filter gender', () => {
     const people = [createMale('man'), createFemale('woman')];
     const numberOfMales = people.filter(p => p instanceof Male).length;
     expect(numberOfMales).to.equal(1);
+  });
+
+  it('loadFromInput', () => {
+    const data = [
+      {gender:'X', name:'people'},
+      {gender:'M', name:'man'},
+      {gender:'F', name:'woman'},
+    ];
+    const people = loadFromInput(data);
+    expect(people[0] instanceof Person).true;
+    expect(people[1] instanceof Male).true;
+    expect(people[2] instanceof Female).true;
   });
 });
