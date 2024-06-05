9409725ab23034813b4f40a9f0a928172f59ca58
StrongWise <ghoul300@naver.com>
2024-06-05 16:10:37 +0900

    feat: replace-subclass-with-delegate

---
+++ src/chap12/replace-subclass-with-delegate-origin.js
@@ -0,0 +1,41 @@
+export class Booking {
+  constructor(show, date) {
+    this._show = show;
+    this._date = date;
+  }
+
+  get hasTalkback() {
+    return this._show.hasOwnProperty('talkback') && !this.isPeakDay;
+  }
+
+  get basePrice() {
+    let result = this._show.price;
+    if (this.isPeakDay) result += Math.round(result * 0.15);
+    return result;
+  }
+
+  get isPeakDay() {
+    const date = new Date(this._date);
+    const month = date.getMonth();
+    return month === 5;
+  }
+}
+
+export class PremiumBooking extends Booking {
+  constructor(show, date, extras) {
+    super(show, date);
+    this._extras = extras;
+  }
+
+  get hasTalkback() {
+    return this._show.hasOwnProperty('talkback');
+  }
+
+  get basePrice() {
+    return Math.round(super.basePrice + this._extras.premiumFee);
+  }
+
+  get hasDinner() {
+    return this._extras.hasOwnProperty('dinner') && !this.isPeakDay;
+  }
+}
