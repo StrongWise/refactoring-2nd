2b898f8bea149246cede9d47d7389c0d914fccfc
StrongWise <ghoul300@naver.com>
2024-06-05 14:35:51 +0900

    refactor: pull up field

--- src/chap12/extract-superclass.js
+++ src/chap12/extract-superclass.js
@@ -1,11 +1,13 @@
 export class Party {
+  constructor(name) {
+    this._name = name;
+  }
 }
 
 export class Employee extends Party {
   constructor(name, id, monthlyCost) {
-    super();
+    super(name);
     this._id = id
-    this._name = name
     this._monthlyCost = monthlyCost
   }
 
@@ -28,8 +30,7 @@ export class Employee extends Party {
 
 export class Department extends Party {
   constructor(name, staff) {
-    super();
-    this._name = name;
+    super(name);
     this._staff = staff;
   }
 
