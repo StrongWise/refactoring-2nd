19432319e4e28184f98c3e0f532760b6ebfb2843
StrongWise <ghoul300@naver.com>
2024-06-10 10:23:06 +0900

    feat: replace-superclass-with-delegate

---
+++ test/chap12/replace-superclass-with-delegate.test.js
@@ -0,0 +1,21 @@
+import {CatalogItem, Scroll} from "../../src/chap12/replace-superclass-with-delegate.js";
+import {expect} from "chai";
+import {DateTime} from "luxon";
+
+describe('replace-superclass-with-delegate', () => {
+  it('CatalogItem', () => {
+    const catalogItem = new CatalogItem(1, 'item1', 'revered');
+    const catalogItem2 = new CatalogItem(2, 'item2', 'tag2');
+    expect(catalogItem.hasTag('revered')).true;
+    expect(catalogItem2.hasTag('revered')).false;
+  });
+  it('Scroll ', () => {
+    const scroll = new Scroll(3, 'item3', 'revered', DateTime.fromISO('2024-06-10'));
+    expect(scroll.needCleaning(DateTime.fromISO('2022-07-10'))).true;
+    expect(scroll.needCleaning(DateTime.fromISO('2022-07-11'))).false;
+
+    const scroll2 = new Scroll(4, 'item4', 'tag4', DateTime.fromISO('2024-06-11'));
+    expect(scroll2.needCleaning(DateTime.fromISO('2020-05-02'))).true;
+    expect(scroll2.needCleaning(DateTime.fromISO('2020-05-03'))).false;
+  });
+});
